247dead54a6c18f172a32551b125e1367f34556c
JayveeErandio <jayveeerandio@gmail.com>
2026-05-10 16:31:31 +0800

    Signup All Input Fields

--- components/screens/SignupScreen.js
+++ components/screens/SignupScreen.js
@@ -1,7 +1,9 @@
 import { useContext, useState } from "react";
 import { View, Text, TextInput, Pressable, Image, Alert } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 import { Variables } from "../../Variables";
 import { useNavigation } from "@react-navigation/native";
+import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
 
 export default function SignupScreen() {
   const navigation = useNavigation();
@@ -9,63 +11,113 @@ export default function SignupScreen() {
   const [ID, setID] = useState("");
 
   return (
-    <View>
-      {/* ==== Headline Top ==== */}
-      <View className="bg-[#fef] flex items-center py-8 gap-2">
-        <Image
-          source={require("../../assets/square.png")}
-          className="rounded-full w-20 h-20 my-3"
-        />
-        <Text className="font-bold text-3xl text-[#333] font-serif text-center">
-          Please provide your student number
-        </Text>
-        <Text className="opacity-50 text-sm text-center mx-5">
-          Your student number will be notified to the GCU for them to verify and
-          create your account.
-        </Text>
-      </View>
-
-      {/* ==== Forms ==== */}
-      <View className="p-7 flex gap-5">
-        <View>
-          <Text className="font-bold text-[#333]">STUDENT ID</Text>
-          <TextInput
-            onChangeText={setID}
-            inputMode="numeric"
-            placeholder="e.g. 202310097"
-            className="border rounded-lg px-3 border-[#ccc] text-[#555] bg-[#eee]"
-            placeholderTextColor="#aaa"
+    <KeyboardAwareScrollView
+      contentContainerStyle={{ flexGrow: 1 }}
+      enableOnAndroid={true}
+      extraScrollHeight={150}
+      keyboardShouldPersistTaps="handled"
+    >
+      <SafeAreaView>
+        {/* ==== Headline Top ==== */}
+        <View className="bg-[#fef] flex items-center py-8 gap-2">
+          <Image
+            source={require("../../assets/square.png")}
+            className="rounded-full w-20 h-20 my-3"
           />
+          <Text className="font-bold text-3xl text-[#333] font-serif text-center">
+            Student Registration Verification
+          </Text>
+          <Text className="opacity-50 text-sm text-center mx-5">
+            Submit your student information for verification. The GCU will
+            review your details before creating your account. Please wait for an
+            email notification regarding your application status.
+          </Text>
         </View>
 
-        <Pressable
-          onPress={() => {
-            if (ID != "") {
-              signup(ID);
-              navigation.goBack();
-              Alert.alert(
-                "Notifying GCU",
-                "Your password will be provided via your school emails when the GCU has already approved your request.",
-                [{ text: "OK" }],
-              );
+        {/* ==== Forms ==== */}
+        <View className="p-7 flex gap-5">
+          <View className="gap-1">
+            <Text className="font-bold text-[#333]">LAST NAME</Text>
+            <TextInput
+              placeholder="e.g. Dela Cruz"
+              className="border rounded-lg px-3 border-[#ccc] text-[#555] bg-[#eee]"
+              placeholderTextColor="#aaa"
+            />
+          </View>
+          <View className="gap-1">
+            <Text className="font-bold text-[#333]">FIRST NAME</Text>
+            <TextInput
+              placeholder="e.g. Juan"
+              className="border rounded-lg px-3 border-[#ccc] text-[#555] bg-[#eee]"
+              placeholderTextColor="#aaa"
+            />
+          </View>
+          <View className="gap-1">
+            <Text className="font-bold text-[#333]">YEAR LEVEL</Text>
+            <TextInput
+              inputMode="numeric"
+              placeholder="e.g. 4"
+              className="border rounded-lg px-3 border-[#ccc] text-[#555] bg-[#eee]"
+              placeholderTextColor="#aaa"
+            />
+          </View>
+          <View className="gap-1">
+            <Text className="font-bold text-[#333]">SECTION</Text>
+            <TextInput
+              placeholder="e.g. DW31"
+              className="border rounded-lg px-3 border-[#ccc] text-[#555] bg-[#eee]"
+              placeholderTextColor="#aaa"
+            />
+          </View>
+          <View className="gap-1">
+            <Text className="font-bold text-[#333]">STUDENT NUMBER</Text>
+            <TextInput
+              inputMode="numeric"
+              placeholder="e.g. 202310097"
+              className="border rounded-lg px-3 border-[#ccc] text-[#555] bg-[#eee]"
+              placeholderTextColor="#aaa"
+            />
+          </View>
+          <View className="gap-1">
+            <Text className="font-bold text-[#333]">PASSWORD</Text>
+            <TextInput
+              secureTextEntry={true}
+              autoCapitalize="none"
+              placeholder="Enter your password"
+              className="border rounded-lg px-3 border-[#ccc] text-[#555] bg-[#eee]"
+              placeholderTextColor="#aaa"
+            />
+          </View>
+
+          <Pressable
+            onPress={() => {
+              if (ID != "") {
+                signup(ID);
+                navigation.goBack();
+                Alert.alert(
+                  "Notifying GCU",
+                  "Your password will be provided via your school emails when the GCU has already approved your request.",
+                  [{ text: "OK" }],
+                );
+              }
+            }}
+            className={
+              "bg-[#c6a] rounded-xl p-5  " +
+              (ID == "" ? "opacity-50" : "active:bg-[#b59]")
             }
-          }}
-          className={
-            "bg-[#c6a] rounded-xl p-5  " +
-            (ID == "" ? "opacity-50" : "active:bg-[#b59]")
-          }
-        >
-          <Text className="font-bold text-white w-full text-center text-lg">
-            Request to GCU {"\u27F6"}
-          </Text>
-        </Pressable>
-        <Pressable
-          onPress={() => navigation.goBack()}
-          className="border border-[#ccc] rounded-xl p-4 active:bg-[#eee]"
-        >
-          <Text className="text-[#777] w-full text-center">Go Back</Text>
-        </Pressable>
-      </View>
-    </View>
+          >
+            <Text className="font-bold text-white w-full text-center text-lg">
+              Request to GCU {"\u27F6"}
+            </Text>
+          </Pressable>
+          <Pressable
+            onPress={() => navigation.goBack()}
+            className="border border-[#ccc] rounded-xl p-4 active:bg-[#eee]"
+          >
+            <Text className="text-[#777] w-full text-center">Go Back</Text>
+          </Pressable>
+        </View>
+      </SafeAreaView>
+    </KeyboardAwareScrollView>
   );
 }
